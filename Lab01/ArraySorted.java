70f60382b302eeb8d9d5b69682d2e5d036be87a7
Nguyễn Viết Minh <92623836+Min-KiD@users.noreply.github.com>
2023-04-20 11:20:11 +0700

    Create ArraySorted.java

---
+++ Lab01/ArraySorted.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ArraySorted{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int [] a = new int [n];
+		int sum = 0;
+		for(int i=0; i < n; i++) {
+			a[i] = sc.nextInt();
+			sum += a[i];
+		}
+		Arrays.sort(a);
+		double avg = (double)sum/n;
+		System.out.println("The sorted array is: "+ Arrays.toString(a));
+		System.out.println("The sum of array is: "+ sum);
+		System.out.println("The average of array is: "+ avg);
+		sc.close();
+   }
+}
