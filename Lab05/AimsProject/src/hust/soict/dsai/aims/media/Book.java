96c98bf75492524d77ade6cf62131f0357c4bc06
Nguyễn Viết Minh <92623836+Min-KiD@users.noreply.github.com>
2023-06-25 23:32:29 +0700

    Add files via upload

---
+++ Lab05/AimsProject/src/hust/soict/dsai/aims/media/Book.java
@@ -0,0 +1,44 @@
+package hust.soict.dsai.aims.media;
+import java.util.ArrayList;
+
+public class Book extends Media {
+	
+	private ArrayList<String> authors = new ArrayList<String>();
+
+	public ArrayList<String> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(ArrayList<String> authors) {
+		this.authors = authors;
+	}
+	
+	public void addAuthor(String authorName) {
+		if (authors.contains(authorName)){
+			System.out.println("Already have");
+		} else {
+			authors.add(authorName);
+			System.out.println("Have been added");
+		}
+	}
+	
+	
+	public void removeAuthor(String authorName) {
+		if (authors.contains(authorName)){
+			authors.remove(authorName);
+			System.out.println("Have been removed");
+		} else {
+			System.out.println("Not have");
+		}
+	}
+
+	public Book(String title, String category, float cost) {
+		super(title, category, cost);
+	}
+	
+	public Book(String title, float cost) {
+		super(title, cost);
+	}
+	
+
+}
